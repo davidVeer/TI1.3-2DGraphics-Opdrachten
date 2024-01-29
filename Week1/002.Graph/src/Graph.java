20585062362f6f6341b110297aa7ac04f8cc5819
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-01-29 11:50:51 +0100

    X^3 grafiekje

--- Week1/002.Graph/src/Graph.java
+++ Week1/002.Graph/src/Graph.java
@@ -9,17 +9,40 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Graph extends Application {
+
+    private Canvas canvas;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Graph");
         primaryStage.show();
     }
-    
-    
+
+
     public void draw(FXGraphics2D graphics) {
+
+        graphics.translate(canvas.getWidth()/2, canvas.getHeight()/2);
+        graphics.scale( 1, -1);
+
+        graphics.setColor(Color.red);
+        graphics.drawLine(-1000,0,1000,0);
+        graphics.setColor(Color.green);
+        graphics.drawLine(0,-1000,0,1000);
+        graphics.setColor(Color.black);
+
+        double resolution = 0.1;
+        double scale = 50.0;
+        double lastY = Math.sin(-10);
+
+        for(double x = -1000; x < 1000; x += resolution)
+        {
+            float y = (float)Math.pow(x,3);
+            graphics.draw(new Line2D.Double(x*scale, y*scale, (x-resolution)*scale, lastY*scale));
+            lastY = y;
+        }
     }
     
     
