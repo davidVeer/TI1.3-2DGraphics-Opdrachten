5c0531bca0ffed96bb70f7ee066158eedf585c6f
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-01-29 11:09:53 +0100

    draw a housie

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -17,7 +17,14 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
-        // test
+        graphics.drawLine(100,200,100,400);
+        graphics.drawLine(400,200,400,400);
+        graphics.drawLine(100,400,400,400);
+        graphics.drawLine(100,200,250,50);
+        graphics.drawLine(400,200,250,50);
+
+        drawRect(150,400,200,300,graphics);
+        drawRect(250,250,350,300,graphics);
     }
 
 
@@ -26,4 +33,13 @@ public class House extends Application {
         launch(House.class);
     }
 
+
+    public void drawRect(int x1, int y1, int x2, int y2, FXGraphics2D graphics){
+        graphics.drawLine(x1, y1, x1, y2);
+        graphics.drawLine(x1, y1, x2, y1);
+        graphics.drawLine(x2, y1, x2, y2);
+        graphics.drawLine(x1, y2, x2, y2);
+    }
 }
+
+
