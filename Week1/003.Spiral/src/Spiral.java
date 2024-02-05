ae6504cca76bbafbc8e1a59922adc509063702c4
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-02-05 11:17:13 +0100

    spiraal tekenen

--- Week1/003.Spiral/src/Spiral.java
+++ Week1/003.Spiral/src/Spiral.java
@@ -9,9 +9,11 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Spiral extends Application {
+
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Spiral");
@@ -20,6 +22,29 @@ public class Spiral extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+        graphics.scale( 1, -1);
+
+        graphics.setColor(Color.red);
+        graphics.drawLine(-1000,0,1000,0);
+        graphics.setColor(Color.green);
+        graphics.drawLine(0,-1000,0,1000);
+        graphics.setColor(Color.black);
+
+        double resolution = 0.1;
+        double scale = 50.0;
+        double n = 1;
+        double lastY = 0;
+        double lastX = 0;
+
+        for(double i = 0; i < 10; i += resolution)
+        {
+            double x = n * i * Math.cos(i);
+            double y = n * i * Math.sin(i);
+            graphics.draw(new Line2D.Double(x*scale, y*scale, lastX*scale, lastY*scale));
+            lastX = x;
+            lastY = y;
+        }
     }
     
     
