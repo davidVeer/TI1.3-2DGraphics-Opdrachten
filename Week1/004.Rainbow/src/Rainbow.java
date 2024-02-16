1d82a0dad7a4f08629999b4e3ad8a830d102f893
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-02-16 11:06:12 +0100

    regenboog tekenen

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -9,9 +9,11 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Rainbow extends Application {
+
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Rainbow");
@@ -20,7 +22,30 @@ public class Rainbow extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+        graphics.scale( 1, -1);
 
+        graphics.setColor(Color.red);
+        graphics.drawLine(-1000,0,1000,0);
+        graphics.setColor(Color.green);
+        graphics.drawLine(0,-1000,0,1000);
+        graphics.setColor(Color.black);
+
+        float radiusBinnen = 100;
+        float radiusBuiten = 500;
+        float hoek;
+
+        for(int i = 0; i < 1885; i++) {
+            graphics.setColor(Color.getHSBColor(i/1885.0f, 1, 1));
+            hoek = i/600.0f;
+            float x1 = (float) (radiusBinnen * Math.cos(hoek));
+            float y1 = (float) (radiusBinnen * Math.sin(hoek));
+            float x2 = (float) (radiusBuiten * Math.cos(hoek));
+            float y2 = (float) (radiusBuiten * Math.sin(hoek));
+
+            graphics.drawLine((int) x1,(int) y1,(int) x2,(int) y2);
+
+        }
     }
     
     
