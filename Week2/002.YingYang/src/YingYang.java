617baf501bfed457e14ecaacd45f2bd1055e5116
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-03-04 11:19:52 +0100

    drawing a YinGYang

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -8,6 +8,8 @@ import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.FillRule;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -27,12 +29,30 @@ public class YingYang extends Application {
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
 
+    public GeneralPath yang(){
+        GeneralPath path = new GeneralPath();
+        path.moveTo(300,100);
+        path.curveTo(400,100,400,300,300,300);
+        path.curveTo(200,300,200,500,300,500);
+        path.curveTo(575,500,575,100,300,100);
+        path.moveTo(300,100);
+        path.closePath();
+        return path;
+    }
 
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
-        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.setColor(Color.BLACK);
+        graphics.draw(new Ellipse2D.Double(100,100,400,400));
+        graphics.fill(yang());
+
+        graphics.setColor(Color.WHITE);
+        graphics.fill(new Ellipse2D.Double(275,375,50,50));
+        graphics.setColor(Color.BLACK);
+        graphics.fill(new Ellipse2D.Double(275,175,50,50));
+
     }
 
 
