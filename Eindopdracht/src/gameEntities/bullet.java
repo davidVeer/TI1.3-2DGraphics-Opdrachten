e006fc0c0dadd26a63fb826b8939f4ff0618d574
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-04-01 00:06:42 +0200

    adding a bullet class adding better sprites

---
+++ Eindopdracht/src/gameEntities/bullet.java
@@ -0,0 +1,23 @@
+package gameEntities;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class bullet
+        implements GameEntity
+{
+    @Override
+    public void draw(Graphics2D graphics) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void initialiseAnimations(BufferedImage spriteSheet, int spriteDimentions) {
+
+    }
+}
